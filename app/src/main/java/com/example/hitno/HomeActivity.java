7419cc79043e43fd282262226d8bca4e066a06a1
apribu <ante.priba123@gmail.com>
2024-01-18 15:43:26 +0100

    Search

--- app/src/main/java/com/example/hitno/HomeActivity.java
+++ app/src/main/java/com/example/hitno/HomeActivity.java
@@ -1,14 +1,79 @@
 package com.example.hitno;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import java.util.ArrayList;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+
 
 public class HomeActivity extends AppCompatActivity {
+    private ArrayList<ExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        createExampleList();
+        buildRecyclerView();
+
+        EditText editText = findViewById(R.id.edittext);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
+    }
+
+    private void filter(String text) {
+        ArrayList<ExampleItem> filteredList = new ArrayList<>();
+
+        for (ExampleItem item : mExampleList) {
+            if (item.getText1().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+
+        mAdapter.filterList(filteredList);
+    }
+    private void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.baseline_stolar_24, "stolar", "50$/h"));
+        mExampleList.add(new ExampleItem(R.drawable.baseline_vodoinstalater_24, "vodoinstalater", "48$/h"));
+        mExampleList.add(new ExampleItem(R.drawable.baseline_elektricar_24, "Line 5", "Line 6"));
+    }
+
+    private void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
+
+
